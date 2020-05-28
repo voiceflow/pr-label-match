13f0c980b2d38d6c3445e5aa467b277bdccc76a9
Frank Gu <frank@voiceflow.com>
2020-05-28 15:01:04 -0400

    test

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ import {checkList, IResult} from './matcher'
 async function run(): Promise<void> {
   try {
     // Input
-    // const re = new RegExp(core.getInput('regex'))
+    const re = new RegExp(core.getInput('regex'))
 
-    core.debug('hello world!')
-    // // Regex check
-    // const PRLabels = github?.context?.payload?.pull_request?.labels
-    // const result: IResult = checkList(PRLabels, re)
+    // Regex check
+    const PRLabels = github?.context?.payload?.pull_request?.labels
+    console.log(PRLabels)
+    const result: IResult = checkList(PRLabels, re)
 
-    // // Outputs
-    // core.setOutput('multiple', result.multiple)
-    // core.setOutput('label', result.label)
+    // Outputs
+    core.setOutput('multiple', result.multiple)
+    core.setOutput('label', result.label)
   } catch (error) {
     core.setFailed(error.message)
   }
